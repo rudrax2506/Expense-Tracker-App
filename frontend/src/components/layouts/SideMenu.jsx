eb1afeeed8ed875fedaed2dca7c6dab41578701d
rudrax2506 <115683707+rudrax2506@users.noreply.github.com>
2025-07-07 23:46:07 +0930

    fixing deployment errors

--- frontend/src/components/layouts/SideMenu.jsx
+++ frontend/src/components/layouts/SideMenu.jsx
@@ -35,7 +35,7 @@ const SideMenu = ({ activeMenu }) => {
           />
         ) : (
           <CharAvatar
-            fullName={fullname}
+            fullName={user?.fullName}
             width="w-20"
             height="h-20"
             style="text-xl"
